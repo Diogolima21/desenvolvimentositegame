e4b534d4da182b663989a89750588ba19d114c53
diogo lima <diogoln1985@gmail.com>
2021-11-25 13:28:41 -0300

    alteração games

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HeaderComponent } from './views/header/header.component';
+import { HomeComponent } from './/views/home/home.component';
+import { JogosComponent } from './views/jogos/jogos.component'; 
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {
+    path:'', 
+    component: HomeComponent,
+  },
+
+  {
+    path:'jogos',
+    component: JogosComponent,
+  }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,18 +4,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatIconModule} from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 
 import { HeaderComponent } from './views/header/header.component';
 import { FooterComponent } from './views/footer/footer.component';
+import { HomeComponent } from './views/home/home.component';
+import { JogosComponent } from './views/jogos/jogos.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    HomeComponent,
+    JogosComponent
   ],
   imports: [
     BrowserModule,
@@ -23,6 +28,7 @@ import { FooterComponent } from './views/footer/footer.component';
     BrowserAnimationsModule,
     MatToolbarModule,
     MatIconModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
